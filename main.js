ea4e288026646d9551d672f0ee3fc8f90ef60161
Nathan Irwin <nki818@mac.com>
2015-01-28 15:05:04 -0800

    added initial key functions

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Nathan Irwin
+ * CMPM 20
+ * Assignment 2
+ */
+use2D = true;
+initGame("canvas");
+
+gInput.addBool(37, "left");
+gInput.addBool(38, "up");
+gInput.addBool(39, "right");
+gInput.addBool(40, "down");
